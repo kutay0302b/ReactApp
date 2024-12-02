735876f70c6571b1266990988e3898b02756f3cc
kutaytoren <kutaytoren@3eendustriyel.com.tr>
2024-12-02 11:07:23 +0300

    Add project files.

---
+++ ReactApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactApp.Startup))]
+namespace ReactApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
